f181f94e0a237b10d7c437034853ee659b8a3e74
Dharren09 <dharrydharrenzerah@gmail.com>
2023-04-18 14:00:12 +0300

    javascript promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+export default function getResponseFromAPI() {
+  const url = 'https://api.spacexdata.com/v4/launches/latest';
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+	throw new Error('Not exact reponse returned');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:' , error);
+      throw error;
+    });
+}
