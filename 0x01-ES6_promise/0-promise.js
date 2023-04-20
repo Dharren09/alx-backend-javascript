eedabd2633e79e17be4c0237a616b84b4863f70a
Dharren09 <dharrydharrenzerah@gmail.com>
2023-04-20 12:50:10 +0000

    javascript promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,7 @@
-const fetch = require('node-fetch');
-
 export default function getResponseFromAPI() {
-  const url = 'https://api.spacexdata.com/v4/launches/latest';
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Not exact reponse returned');
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-      throw error;
-    });
+  const response = new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+  return response;
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  const response = [];
+  try {
+    const createUser = await signUpcreatedUser(firstName, lastName);
+    response.push({ status: 'fullfilled', value: createdUser });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    response.push({
+      status: 'rejected',
+      value: 'Error: ${fileName} cannot be processed',
+    });
+  }
+  return response;
 }
