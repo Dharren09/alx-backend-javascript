d2a77591fb82aea67a2fd5a77adc0ad9de380445
Dharren09 <dharrydharrenzerah@gmail.com>
2023-04-25 18:37:28 +0300

    Javascript Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
-import signUpUser from './4-user-promise';
+import signUpcreatedUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const response = [];
   try {
-    const createUser = await signUpcreatedUser(firstName, lastName);
-    response.push({ status: 'fullfilled', value: createdUser });
+    const createdUser = await signUpcreatedUser(firstName, lastName);
+    response.push({ status: 'fulfilled', value: createdUser });
     await uploadPhoto(fileName);
   } catch (err) {
     response.push({
       status: 'rejected',
-      value: 'Error: ${fileName} cannot be processed',
+      value: `Error: ${fileName} cannot be processed`,
     });
   }
   return response;
